31562910de2abd7617d33139cf59b6ea7a6412c4
OroTC <87296816+orotc@users.noreply.github.com>
2021-07-13 00:16:50 +0800

    Update stars.rs

--- stars.rs
+++ stars.rs
@@ -1,1 +1,12 @@
+use byteorder::{ByteOrder, LittleEndian, ReadBytesExt, WriteBytesExt};
+use crc::Crc;
+use crc::CRC_32_ISCSI;
+use std::{
+    error::Error,
+    fmt, io,
+    io::{Read, Seek, SeekFrom, Write},
+};
 
+fn mask_crc(crc: u32) -> u32 {
+    ((crc >> 15) | (crc << 17)).wrapping_add(0xa282_ead8u32)
+}
